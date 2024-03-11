503e849e4fc1e71926032ca2c49291bd5757406d
김무준 <power649@naver.com>
2024-03-11 17:06:33 +0900

    [init] Backend 프로젝트 init

---
+++ ForMyBaby/backend/src/main/java/com/ssafy/c202/ForMyBabyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.c202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForMyBabyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForMyBabyApplication.class, args);
+	}
+
+}
