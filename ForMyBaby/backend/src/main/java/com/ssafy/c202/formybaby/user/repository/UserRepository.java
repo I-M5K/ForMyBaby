905c8633e65a6e2f51402c9d88fa65df09eeb5c0
dodokim98 <eh6848zzz@gmail.com>
2024-03-20 10:47:07 +0900

    feat: 카카오 로그인 시 유저 정보 반환 기능 추가

--- ForMyBaby/backend/src/main/java/com/ssafy/c202/formybaby/user/repository/UserRepository.java
+++ ForMyBaby/backend/src/main/java/com/ssafy/c202/formybaby/user/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.ssafy.c202.formybaby.user.repository;
 
 import com.ssafy.c202.formybaby.user.entity.User;
+import com.ssafy.c202.formybaby.user.mapper.UserInfoMapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<UserInfoMapper> findUserInfoByOauth_OauthId(Long oauthId);
+
     User findByUserId(Long userId);
 }
