139a3bc716e6106922a22e61d1fc60f1ecb6ea26
dodokim98 <eh6848zzz@gmail.com>
2024-03-21 09:46:24 +0900

    fix: 카카오 로그인 시 유저 정보 반환 기능 수정

--- ForMyBaby/backend/src/main/java/com/ssafy/c202/formybaby/user/mapper/UserInfoMapper.java
+++ ForMyBaby/backend/src/main/java/com/ssafy/c202/formybaby/user/mapper/UserInfoMapper.java
@@ -2,8 +2,4 @@ package com.ssafy.c202.formybaby.user.mapper;
 
 public interface UserInfoMapper {
     Long getUserId();
-<<<<<<< HEAD
-
-=======
->>>>>>> feat/#628-backend-user
 }
--- ForMyBaby/backend/src/main/java/com/ssafy/c202/formybaby/user/repository/UserRepository.java
+++ ForMyBaby/backend/src/main/java/com/ssafy/c202/formybaby/user/repository/UserRepository.java
@@ -8,11 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-<<<<<<< HEAD
     Oauth findByOauth_OauthId(Long oauthId);
-=======
     Optional<UserInfoMapper> findUserInfoByOauth_OauthId(Long oauthId);
-
->>>>>>> feat/#628-backend-user
     User findByUserId(Long userId);
 }
