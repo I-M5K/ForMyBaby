1b814cdcf9c9af42a11bb39633123fd2b7385a99
dodokim98 <eh6848zzz@gmail.com>
2024-03-22 11:18:06 +0900

    chore: 패키지 구조 수정- redis 관련

---
+++ ForMyBaby/backend/src/main/java/com/ssafy/c202/formybaby/global/redis/RedisService.java
@@ -0,0 +1,21 @@
+package com.ssafy.c202.formybaby.global.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public void setValue(String key, String value) {
+        stringRedisTemplate.opsForValue().set(key, value);
+    }
+
+    public String getValue(String key) {
+        return stringRedisTemplate.opsForValue().get(key);
+    }
+}
+
