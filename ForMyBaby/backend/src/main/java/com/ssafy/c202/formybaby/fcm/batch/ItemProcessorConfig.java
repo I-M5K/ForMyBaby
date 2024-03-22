239c91eff5c610b4d62800d90ae887100fb1ea12
김무준 <power649@naver.com>
2024-03-22 10:21:08 +0900

    [feat] Spring Batch 설계중

---
+++ ForMyBaby/backend/src/main/java/com/ssafy/c202/formybaby/fcm/batch/ItemProcessorConfig.java
@@ -0,0 +1,24 @@
+package com.ssafy.c202.formybaby.fcm.batch;
+
+import com.ssafy.c202.formybaby.health.entity.Health;
+import com.ssafy.c202.formybaby.notification.entity.Notification;
+import com.ssafy.c202.formybaby.vaccine.entity.Vaccine;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ItemProcessorConfig {
+
+    @Bean
+    ItemProcessor<Health, Notification> checkHealthProcessor() {
+        return null;
+    }
+
+    @Bean
+    ItemProcessor<Vaccine, Notification> checkVaccineProcessor() {
+        return null;
+    }
+}
