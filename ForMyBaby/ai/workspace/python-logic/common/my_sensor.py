a95aef6a51ecc8ed5281071f48d337fd847d50f0
임채진 <chaejinims2@gmail.com>
2024-03-22 17:42:27 +0900

    feat: 소리 데이터(데시벨) 수집 기능 추가

--- ForMyBaby/ai/workspace/python-logic/common/my_sensor.py
+++ ForMyBaby/ai/workspace/python-logic/common/my_sensor.py
@@ -3,10 +3,11 @@ import sys
 import re
 import serial as pyserial
 from io import BytesIO
-
+import pyaudio
+import audioop
+import math
 from collections import namedtuple
 
-# Define a namedtuple class for your sensor data
 SensorData = namedtuple('SensorData', ['aX', 'aY', 'aZ', 'gX', 'gY', 'gZ', 'Tmp', 'dh', 'dt'])
 FeatureData = namedtuple('FeatureData', ['f1', 'f2', 'rtn'])
 
@@ -15,20 +16,17 @@ class MySensor:
         self.name = name
         self.cam = self.webcam_open()
         self.ser = self.serial_connect()
+        self.p = self.sound_open()
 
     def __str__(self):
         return self.name
 
     def webcam_open(self):
         try:
-            webcam = cv2.VideoCapture(0)
-            print('Webcam is opened.')
-            return webcam
+            return cv2.VideoCapture(0)
         except Exception as e:
-            print('Cannot open the webcam.')
-            print(str(e))
+            print(f'Cannot open the webcam: {e}')
             sys.exit(1)
-
 
     def frame_capture(self):
         self.ret, self.frame = self.cam.read()
@@ -36,40 +34,53 @@ class MySensor:
     def frame_encode(self):
         try:
             _, self.jpeg = cv2.imencode('.jpg', self.frame)
-            self.jpeg_bytes = jpeg.tobytes()
-            self.jpeg_file = BytesIO(jpeg_bytes)
-            return self.jpeg_file
+            return BytesIO(self.jpeg.tobytes())
         except Exception as e:
-            print('Cannot encode the frame.')
-            print(str(e))
+            print(f'Cannot encode the frame: {e}')
             return None
+
 
     def webcam_release(self):
         self.cam.release()
         cv2.destroyAllWindows()
 
-            
+    def sound_open(self):
+        try:
+            return pyaudio.PyAudio()
+        except Exception as e:
+            print(f'Cannot open the sound device: {e}')
+            return None
+
+    def sound_get(self):
+        chunk = 1024
+        sample_format = pyaudio.paInt16
+        channels = 1
+        fs = 44100
+        stream = self.p.open(format=sample_format, channels=channels, rate=fs, frames_per_buffer=chunk, input=True)
+        data = stream.read(chunk)
+        return 20 * math.log10(audioop.rms(data, 2))
+
+    def sound_close(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
+        
     def serial_connect(self):
         try:
-            ser = pyserial.Serial('/dev/ttyACM0', 9600)
-            return ser
+            return pyserial.Serial('/dev/ttyACM0', 9600)
         except pyserial.SerialException:
             print("Cannot start serial communication.")
             return None
-        
+
     def data_get(self):
-        # Read line data from serial port
         if self.ser.in_waiting > 0:
             self.line = self.ser.readline().decode('utf-8').rstrip()
 
     def data_raw(self):
         if self.line:
-            # Use regular expressions to parse the sensor values from the line.
             match = re.match(r'(.*) , (.*) , (.*) , (.*) , (.*) , (.*) , (.*) , (.*) , (.*)', self.line)
-
             if match:
-                self.aX, self.aY, self.aZ, self.gX, self.gY, self.gZ, self.Tmp, self.dh, self.dt = map(float, match.groups())
-                self.data = SensorData(self.aX, self.aY, self.aZ, self.gX, self.gY, self.gZ, self.Tmp, self.dh, self.dt)
+                self.data = SensorData(*map(float, match.groups()))
             else:
                 self.data = None
 
@@ -77,8 +88,6 @@ class MySensor:
         self.f1 = (self.data.aX**2 + self.data.aY**2 + self.data.aZ**2)**0.5
         self.f2 = (self.data.gX**2 + self.data.gY**2 + self.data.gZ**2)**0.5
 
-
-        
     def __del__(self):
         self.webcam_release()
         if self.ser:
