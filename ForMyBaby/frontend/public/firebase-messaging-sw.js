a5fab7ece7b44634e24a188f67cfc291013ec207
김광연 <kwang9705@naver.com>
2024-03-22 13:18:28 +0900

    feat: #622-fcm-key-setting

--- ForMyBaby/frontend/public/firebase-messaging-sw.js
+++ ForMyBaby/frontend/public/firebase-messaging-sw.js
@@ -6,11 +6,14 @@ self.addEventListener("install", function (e) {
   self.addEventListener("activate", function (e) {
     console.log("fcm sw activate..");
   });
+
+  var key = ''
   
   self.addEventListener("push", function (e) {
     console.log("push: ", e.data.json());
     if (!e.data.json()) return;
   
+    const data = e.data.json().data;
     const resultData = e.data.json().notification;
     const notificationTitle = resultData.title;
     const notificationOptions = {
@@ -21,12 +24,22 @@ self.addEventListener("install", function (e) {
     };
     console.log("push: ", { resultData, notificationTitle, notificationOptions });
   
+    key = data.key
+    console.log(key);
     self.registration.showNotification(notificationTitle, notificationOptions);
   });
   
   self.addEventListener("notificationclick", function (event) {
     console.log("notification click");
-    const url = "/";
+    var url = "/main";
+    console.log('key: ', key);
+    if (key === 'danger'){ // 위험 감지
+      url = '/baby-guard';
+    } else if (key === 'health'){ // 건강검진
+      url = '/timeline';
+    } else if (key === 'shot'){ // 접종
+      url = '/timeline';
+    }
     event.notification.close();
     event.waitUntil(clients.openWindow(url));
   });
