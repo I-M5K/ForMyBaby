5a1432c86e05562424e59c92bc12484825426db8
김광연 <kwang9705@naver.com>
2024-03-23 18:27:55 +0900

    feat: #0323-socket-setting-allpages

--- ForMyBaby/frontend/src/pages/Features/BabyGuard.jsx
+++ ForMyBaby/frontend/src/pages/Features/BabyGuard.jsx
@@ -8,36 +8,42 @@ const ENDPOINT = 'http://localhost:3001'; // 서버의 엔드포인트에 맞게
 
 const ClientComponent = () => {
   const [imageData, setImageData] = useState(null);
+  const [lineData, setLineData] = useState('');
+  const [babyId, setBabyId] = useState(1); // 기본 아이디 설정
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT, {
       transports: ['websocket'], // WebSocket 프로토콜 강제 사용
     });
 
-    socket.on('image', (data) => {
-      // ArrayBuffer를 Base64로 변환
+    socket.emit('babyId', babyId); // 클라이언트가 babyId를 소켓 서버로 전송
+
+    socket.on('image', ({ imageData, lineData }) => {
+      // ArrayBuffer를 Base64로 변환하여 이미지 데이터 업데이트
       const base64String = btoa(
-        new Uint8Array(data).reduce((data, byte) => data + String.fromCharCode(byte), '')
+        new Uint8Array(imageData).reduce((data, byte) => data + String.fromCharCode(byte), '')
       );
-      // 이미지 데이터를 Base64로 인코딩하여 상태 업데이트
       setImageData(`data:image/jpeg;base64,${base64String}`);
+      // 데이터 업데이트
+      setLineData(lineData);
     });
 
     return () => {
-      socket.disconnect(); // 컴포넌트가 언마운트될 때 소켓 연결 종료
     };
-  }, []); // 페이지가 처음 로드될 때 한 번만 실행
+  }, [babyId]); // babyId가 변경될 때마다 소켓 연결 재설정
 
   return (
     <div className="cctv-container">
       <h1>Received Image</h1>
       {imageData && <img src={imageData} alt="Received" />}
+      {lineData && <p>Received Line: {lineData}</p>}
     </div>
   );
 };
 
 const App = () => {
   const [selectedButton, setSelectedButton] = useState('button1'); // 디폴트로 'button1' 선택
+  const [babyId, setBabyId] = useState('');
 
   const sleepStatus = {
     movement: 80,
@@ -51,6 +57,10 @@ const App = () => {
     setSelectedButton(buttonName);
   };
 
+  const handleBabyIdChange = (event) => {
+    setBabyId(event.target.value);
+  };
+
   return (
     <div className="app">
       <p className="title">우리 아이 지킴이</p>
@@ -59,7 +69,12 @@ const App = () => {
         <button className={selectedButton === 'button2' ? 'bold' : ''} onClick={() => handleButtonClick('button2')}>상세</button>
         <button className={selectedButton === 'button3' ? 'bold' : ''} onClick={() => handleButtonClick('button3')}>변화</button>
       </div>
-      {selectedButton === 'button1' && <ClientComponent />} {/* 대시보드가 선택되었을 때만 ClientComponent를 렌더링 */}
+      {selectedButton === 'button1' && (
+        <>
+          {/* <input type="text" value={babyId} onChange={handleBabyIdChange} placeholder="Enter Baby ID" /> */}
+          <ClientComponent babyId={babyId} />
+        </>
+      )} {/* 대시보드가 선택되었을 때만 ClientComponent를 렌더링 */}
       <div className="sleep-info">
         <p className='sleep-title'>수면 정보</p>
         <div className="sleep-status">
