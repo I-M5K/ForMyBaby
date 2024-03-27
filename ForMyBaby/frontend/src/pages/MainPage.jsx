b7930680a4d0aed3a53ffe7610e3bbe82b666754
junbro1120 <licence22@naver.com>
2024-03-27 14:36:14 +0900

    fix: 메인페이지 CSS 변경

--- ForMyBaby/frontend/src/pages/MainPage.jsx
+++ ForMyBaby/frontend/src/pages/MainPage.jsx
@@ -7,6 +7,12 @@ import { sendLocation } from '../api/userApi';
 import { requestPermission } from "../FCM/firebase-messaging-sw";
 import { useUserStore } from '../stores/UserStore'; // Zustand 스토어 import
 
+import BabyPhoto from '../assets/bangabanga.png'
+import Books from '../assets/books.png'
+import SleepChart from '../assets/sleepChart.png'
+import Syringe from '../assets/syringe.png'
+import PresentBox from '../assets/presentBox.png'
+
 const MainPage = () => {
   const location = useGeoLocation();
   const { id, name, email, fcm, setFcm } = useUserStore();
@@ -28,10 +34,8 @@ const MainPage = () => {
     <div className="container">
       <div className="header">
         <span className="headerText">
-          <>
             지금은 무준이가<br />
             낮잠 잘 시간이에요!
-          </>
         </span>
         <Link to="/notification">
           <div className="notificationIcon">
@@ -42,26 +46,25 @@ const MainPage = () => {
       <img src={require('../assets/babybear.png')} className="gombaImage" alt="Baby Bear" />
       <Link to="/present">
         <div className="rectangleBox">
-          <>
+          <span className="rectangleBoxText">
             100% 채우면 과연 어떤 선물이?
-          </>
+          </span>
         </div>
       </Link>
 
       <div className="boxContainerRight">
-        <Link to="/sleep-pattern">
+        <Link to="/my-page">
           <div className="smallmiddleBox">
-            <span className="boxText">a</span>
+            <img src={BabyPhoto} className='babyphoto' />
           </div>
         </Link>
         <Link to="/sleep-pattern">
           <div className="smallBox">
-            <>
               <span className="boxText">
-                우리아이<br />
-                수면패턴
+                <span className="textSmall">우리아이</span><br />
+                <span className="textLarge">수면패턴</span>
               </span>
-            </>
+                <img src={SleepChart} className='syringe' />
           </div>
         </Link>
       </div>
@@ -69,32 +72,31 @@ const MainPage = () => {
       <div className="boxContainerLeft">
         <Link to="/baby-age">
           <div className="smallBox">
-            <>
               <span className="boxText">
-                무준이가 태어난지<br />
-                27일 되었어요
+                  <span className="textLarge">무준이가 태어난지</span><br />
+                  <span className="textExSmall">
+                    '72'
+                    일 되었어요
+                  </span>
               </span>
-            </>
           </div>
         </Link>
         <Link to="/timeline">
           <div className="smallBox">
-            <>
               <span className="boxText">
-                이번주<br />
-                건강검진
+                <span className="textSmall">이번주</span><br />
+                <span className="textLarge">건강/검진</span>
               </span>
-            </>
+                <img src={Syringe} className='syringe' />
           </div>
         </Link>
         <Link to="/parenting-tips">
           <div className="smallBox">
-            <>
               <span className="boxText">
-                우리아이<br />
-                육아꿀팁
+                <span className="textSmall">우리아이</span><br />
+                <span className="textLarge">육아꿀팁</span>
               </span>
-            </>
+                <img src={Books} className='syringe' />
           </div>
         </Link>
       </div>
