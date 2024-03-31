e633db7f873cf8876f2a199ad2a71192895339e6
김광연 <kwang9705@naver.com>
2024-04-01 00:52:53 +0900

    feat: #0331 get ai and send data

--- ForMyBaby/ai/workspace/express-network/index.js
+++ ForMyBaby/ai/workspace/express-network/index.js
@@ -13,6 +13,8 @@ app.use(express.urlencoded({extended: true}));
 // 방 정보를 저장할 객체
 const rooms = {};
 
+let currentStatus = false; // 초기 상태 설정
+
 // WebSocket 연결
 io.on('connection', (socket) => {
   console.log('A client connected');
@@ -46,6 +48,14 @@ io.on('connection', (socket) => {
       // 이미 해당 아이디와 같은 방이 있으면 해당 방에 소켓 추가
       rooms[babyId].push(socket.id);
       console.log(`Added socket to room for babyId: ${babyId}`);
+    }
+  });
+
+  // 클라이언트로부터 상태 수신 (실시간 영상 데이터가 필요한지 여부 확인용): true면 필요, false면 불필요
+  socket.on('status', (status) => {
+    console.log(`Received status: ${status}`);
+    if (currentStatus != status){
+      currentStatus = status;
     }
   });
 
@@ -89,6 +99,18 @@ const storage = multer.diskStorage({
   }
 });
 
+// 영상 데이터 필요 여부 체크
+app.post('/check', (req, res) => {
+  try {
+    const { timestamp } = req.body;
+    console.log(`실시간 영상 필요 여부 체크: ${timestamp}`);
+    res.status(200).send(currentStatus);
+  } catch{
+    console.error(error);
+    res.status(500).send('체크 에러!');
+  }
+})
+
 
 // 이미지, 시간, 온습도를 받을 라우트 설정
 const upload = multer({ storage: storage });
@@ -96,121 +118,142 @@ app.post('/data', upload.single('image'), (req, res) => {
   try {
     // const babyId = req.params.babyId;
     //console.log('Received image for babyId:', babyId);
+    const imageFile = req.file;
+    const body = req.body; // line 데이터 수신
 
-    const line = req.body.line; // line 데이터 수신
-    console.log('Received line:', line);
+    // 받은 데이터 확인
+    console.log('Received image:', imageFile);
+    console.log('Received data:', body);
 
-    const timestamp = req.body.timestamp; // timestamp 데이터 수신
-    const datetime = req.body.datetime; // timestamp 데이터 수신
+    // 시간과 온습도 등의 데이터 추출
+    const babyId = body.babyId;
+    const timestamp = body.timestamp;
+    const temperature = body.TH ? body.TH[0] : null;
+    const humidity = body.TH ? body.TH[1] : null;
+
+    console.log('Received babyId:', babyId);
     console.log('Received timestamp:', timestamp);
-
-
-    console.log(datetime, line); // Combine into one line
+    console.log('Received temperature:', temperature);
+    console.log('Received humidity:', humidity);
 
     // 이미지 파일을 읽어서 데이터를 클라이언트로 전송
-    fs.readFile(req.file.path, (err, data) => {
-      if (err) throw err;
-
+    fs.readFile(imageFile.path, (err, data) => {
+      if (err) {
+        console.error('Error reading image file:', err);
+        res.status(500).send('이미지 파일 읽기 실패!!');
+        return;
+      }
       // 이미지와 데이터를 방에 소속된 모든 소켓에게 전송
-      io.emit('image', { imageData: data, lineData: line, timestamp: datetime });
-
+      io.emit('image', { imageData: data, babyId: babyId, timestamp: timestamp, temp: temperature, humid: humidity });
+      // 이미지 데이터를 성공적으로 전송한 후에는 응답을 클라이언트에게 보냅니다.
+      res.status(200).send('실시간 영상 전송 성공!');
     });
-    res.status(200).send('Image received');
   } catch (error) {
     console.error(error);
-    res.status(500).send('An error occurred while processing the image.');
+    res.status(500).send('실시간 영상 전송 실패!');
   }
 });
 
-// 이벤트 (일반 - 스탬프용)
-// timestamp; event_type; device_model; device_ID; baby_id
-// '/event/:babyId'
+// 이벤트 
 app.post('/event', (req, res) => {
   try {
-    // const babyId = req.params.babyId;
-    console.log(req.body);
-    const data = req.body;
-    const timestamp = data.timestamp;
-    const event_type = data.event_type;
-    const device_model = data.device_model;
-    const device_ID = data.device_ID;
-
-
-    // 받은 데이터 로깅
-    //console.log(`Received sleep event for babyId: ${babyId}`);
-    console.log(`Timestamp: ${timestamp}, Event type: ${event_type}, Device model: ${device_model}, Device ID: ${device_ID}`);
-
-    // 해당 방에 속한 클라이언트들에게 데이터 전송
-    io.emit('commonEvent', { timestamp, event_type, device_model, device_ID });
-
-    res.status(200).send('일반 스탬프용 데이터 전송 완료!');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('일반 스탬프용 데이터 전송 실패!');
-  }
-});
-
-
-// 이벤트 (위험 - 알림용)
-// timestamp; event_type; device_model; device_ID; baby_id
-// '/danger/:babyId'
-app.post('/danger', (req, res) => {
-  try {
-    console.log(req.body.data)
-    if (!req.body) {
-      throw new Error('Request body is undefined or empty');
-    }
-
-    const { timestamp, event_type, device_model, device_ID } = req.body;
-
-    if (!timestamp || !event_type || !device_model || !device_ID) {
-      throw new Error('One or more required fields are missing in the request body');
-    }
-
-    // 받은 데이터 로깅
-    console.log('Received data:');
-    console.log(`Timestamp: ${timestamp}, Event type: ${event_type}, Device model: ${device_model}, Device ID: ${device_ID}`);
-
-    // 해당 방에 속한 클라이언트들에게 데이터 전송
-    io.emit('dangerEvent', { timestamp, event_type, device_model, device_ID });
-
-    res.status(200).send('위험 알림용 데이터 전송 완료!');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('위험 알림용 데이터 전송 실패!');
-  }
-});
-
-
-// 이벤트 (수면 - 분석용)
-// timestamp; event_type; device_model; device_ID; baby_id
-// '/sleep/:babyId'
-app.post('/sleep', (req, res) => {
-  try {
     //const babyId = req.params.babyId;
-    const { timestamp, event_type, device_model, device_ID } = req.body;
+    const { babyId, timestamp, event_type, url_s3, detail } = req.body;
 
     // 받은 데이터 로깅
     //console.log(`Received sleep event for babyId: ${babyId}`);
-    console.log(`Timestamp: ${timestamp}, Event type: ${event_type}, Device model: ${device_model}, Device ID: ${device_ID}`);
+    console.log(`babyId: ${babyId}, Timestamp: ${timestamp}, Event type: ${event_type}, detail: ${detail}, url s3: ${url_s3}`);
 
-    // 해당 방에 속한 클라이언트들에게 데이터 전송
-    io.emit('sleepEvent', { timestamp, event_type, device_model, device_ID });
-
-    res.status(200).send('수면 분석용 데이터 전송 완료!');
+    if (event_type == '2'){ // 수면 관련
+      if (detail == '0'){ // 잠에서 깸
+        io.emit('sleepEvent', { timestamp, detail, babyId});
+      } else if (detail == '1') { // 잠 듦
+        io.emit('sleepEvent', { timestamp, detail, babyId});
+      }
+      res.status(200).send('수면 분석 데이터 전송 완료!');
+    } else if (event_type == '1'){ // 위험 관련
+      if (detail == '0'){ // 위험행동 - 뒤집기(이전)
+        io.emit('dangerEvent', {timestamp, detail, babyId });
+      }
+      res.status(200).send('위험 행동 데이터 전송 완료!');
+    } else { // 스톱모션(0), 만세(1), 다리꼬기(2) 
+      if (detail == '0'){ // 스톱모션
+        io.emit('commonEvent', { timestamp, detail, babyId, s3_url });
+      } else if (detail == '1'){ // 만세
+        io.emit('commonEvent', { timestamp, detail, babyId, s3_url });
+      } else if (detail == '2'){ // 다리꼬기
+        io.emit('commonEvenet', { timestamp, detail, babyId, s3_url });
+      }
+      res.status(200).send('이벤트 행동 데이터 전송 완료!');
+    }       
   } catch (error) {
     console.error(error);
-    res.status(500).send('수면 분석용 데이터 전송 실패!');
+    res.status(500).send('데이터 전송 실패!');
   }
 });
-
-
-
-
-
 
 // 서버 실행
 const PORT = process.env.PORT || 3001;
 http.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+// 이벤트 (일반 - 스탬프용)
+// timestamp; event_type; device_model; device_ID; baby_id
+// '/event/:babyId'
+// app.post('/event', (req, res) => {
+//   try {
+//     // const babyId = req.params.babyId;
+//     console.log(req.body);
+//     const data = req.body;
+//     const timestamp = data.timestamp;
+//     const event_type = data.event_type;
+//     const device_model = data.device_model;
+//     const device_ID = data.device_ID;
+
+
+//     // 받은 데이터 로깅
+//     //console.log(`Received sleep event for babyId: ${babyId}`);
+//     console.log(`Timestamp: ${timestamp}, Event type: ${event_type}, Device model: ${device_model}, Device ID: ${device_ID}`);
+
+//     // 해당 방에 속한 클라이언트들에게 데이터 전송
+//     io.emit('commonEvent', { timestamp, event_type, device_model, device_ID });
+
+//     res.status(200).send('일반 스탬프용 데이터 전송 완료!');
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('일반 스탬프용 데이터 전송 실패!');
+//   }
+// });
+
+
+// 이벤트 (위험 - 알림용)
+// timestamp; event_type; device_model; device_ID; baby_id
+// '/danger/:babyId'
+// app.post('/danger', (req, res) => {
+//   try {
+//     console.log(req.body.data)
+//     if (!req.body) {
+//       throw new Error('Request body is undefined or empty');
+//     }
+
+//     const { timestamp, event_type, device_model, device_ID } = req.body;
+
+//     if (!timestamp || !event_type || !device_model || !device_ID) {
+//       throw new Error('One or more required fields are missing in the request body');
+//     }
+
+//     // 받은 데이터 로깅
+//     console.log('Received data:');
+//     console.log(`Timestamp: ${timestamp}, Event type: ${event_type}, Device model: ${device_model}, Device ID: ${device_ID}`);
+
+//     // 해당 방에 속한 클라이언트들에게 데이터 전송
+//     io.emit('dangerEvent', { timestamp, event_type, device_model, device_ID });
+
+//     res.status(200).send('위험 알림용 데이터 전송 완료!');
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('위험 알림용 데이터 전송 실패!');
+//   }
+// });
