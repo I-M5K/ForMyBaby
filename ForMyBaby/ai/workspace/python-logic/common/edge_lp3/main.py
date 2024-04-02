9f65d06d050ed0398e02a4c8353ae6f29f45bcf2
dodokim98 <eh6848zzz@gmail.com>
2024-04-02 09:29:18 +0900

    fix : dockerfiles

--- ForMyBaby/ai/workspace/python-logic/common/edge_lp3/main.py
+++ ForMyBaby/ai/workspace/python-logic/common/edge_lp3/main.py
@@ -4,7 +4,6 @@ from edge_LP import NetworkManager
 from my_main import EdgeLP
 
 def main():
-
     edge_lp_config = {
         # "ip": "192.168.100.58", # kky
         # "ip": "192.168.100.130", # icj
