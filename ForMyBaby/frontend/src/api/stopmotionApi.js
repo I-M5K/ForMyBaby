3b55b7b122933bcb99095cbb36c1df15722f1a12
김광연 <kwang9705@naver.com>
2024-04-03 23:18:18 +0900

    fix: #0403 s3 url2

--- ForMyBaby/frontend/src/api/stopmotionApi.js
+++ ForMyBaby/frontend/src/api/stopmotionApi.js
@@ -4,7 +4,7 @@ export const getMotionCnt = async () => {
     console.log('스톱모션 사진 수 가져오기!');
     try {
       const response = await axiosWrapper.get('/v1/stopmotion/count');
-      return response.data; // 개수 반환
+      return response.data.imageCount; // 개수 반환
     } catch (error) {
       console.error('스톱 모션 사진 수 가져오는데 에러 발생:', error);
       throw error; // 에러를 다시 던져서 상위 레벨에서 처리하도록 함
@@ -22,10 +22,10 @@ export const getPresentUrl = async () => {
     }
 };
 
-export const sendMotionUrl = async (babyId) => {
+export const sendMotionUrl = async (imageUrl) => {
   console.log('스톱모션 사진 url 보내기!');
   try {
-    const response = await axiosWrapper.get(`/v1/stopmotion/create/${babyId}`);
+    const response = await axiosWrapper.get(`/v1/stopmotion/create`, { params: { imageUrl: imageUrl } });
     return response.data; // url 반환
   } catch (error) {
     console.error('스톱 모션 영상 url 가져올 때 에러 발생:', error);
