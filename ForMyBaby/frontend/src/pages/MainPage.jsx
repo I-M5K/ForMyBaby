e1f237de2359eb9dae323b4949631b6f6e0c05ee
junbro1120 <licence22@naver.com>
2024-04-04 09:43:21 +0900

    fix: 메인 day

--- ForMyBaby/frontend/src/pages/MainPage.jsx
+++ ForMyBaby/frontend/src/pages/MainPage.jsx
@@ -223,7 +223,7 @@ const MainPage = () => {
               <span className="textMiddle">{selectedBabyName} 태어난지</span>
               <br />
               <span className="textExSmall">
-                <div className="babyday">{selectedBabyDay}일 되었어요</div>
+                <div className="babyday">{selectedBabyDay}</div>일 되었어요
               </span>
             </span>
           </div>
